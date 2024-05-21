0708ef649034474ad6f1efcae786e0add4dc8f90
JAI-PHX <JAI2150065@MARICOPA.EDU>
2024-05-21 16:28:43 -0700

    Updated all of the files so the code and the comments would be easier to read and work as intended

--- GamePlay.java
+++ GamePlay.java
@@ -2,17 +2,16 @@ import java.util.Scanner;
 
 public class GamePlay {
 
-    // Private field of a Person.
+    // Private field
     private Person player;
 
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
 
-        // Create a new instance of GamePlay.
         GamePlay gamePlay = new GamePlay();
 
-        // Prompt the user for their first and last name.
+        // Ask the user for the first and last name
         System.out.print("What is your first name?\n");
 
         String first_name = scanner.nextLine();
@@ -21,9 +20,7 @@ public class GamePlay {
 
         String last_name = scanner.nextLine();
 
-        // Create an instance of a Person based on user input when prompted for their last name.
-        // Evaluates user input of last name. If the user did not user input their last name. It creates an instance of a Person using only their first name.
-        // If the user inputs their last name. It will create an instance of a Person using their first and last name
+        // Create an instance of a Person first and last name based on their input
         if (last_name.isEmpty()) {
 
             gamePlay.player = new Person(first_name);
@@ -33,16 +30,16 @@ public class GamePlay {
             gamePlay.player = new Person(first_name, last_name);
         }
 
-        // Create a new instance of the Numbers class and generate a random number.
+        // Create a Numbers class and generate a random number
         Numbers game = new Numbers();
 
-        // Generate a random number from 0 to 100.
+        // Generate a number from 0 to 100
         game.generateNumber();
 
         // Get full name.
         String fullName = gamePlay.player.getpersonsFirstname() + (gamePlay.player.getpersonsLastname().isEmpty() ? "" : " " + gamePlay.player.getpersonsLastname());
 
-        // Loop to prompt the user by name to enter a guess.
+        // Create a loop to ask the user by to enter their guess
         boolean rightGuess = false;
 
         while (!rightGuess) {
@@ -51,7 +48,7 @@ public class GamePlay {
 
             int guess = scanner.nextInt();
 
-            // Compare the guess generated to the users guess
+            // Compare the users guess to the generated guess
             rightGuess = game.compareNumber(guess);
         }
 
--- Numbers.java
+++ Numbers.java
@@ -5,54 +5,53 @@ public class Numbers {
     private int randomNum;
 
     // One public getter and one public setter for randomNum
-    // Public int getrandomNum() is a getter for the random number, which returns the value of randomNum.
+
+    // Getter for randomNum
     public int getRandomNum() {
 
         return randomNum;
     }
 
-    // Public void setrandomNum(int randomNum) is a setter for the randomNum.
-    // This setter sets the value and parameter of the randomNum field.
+    // This setter randomNum
     public void setRandomNum(int randomNum) {
 
         this.randomNum = randomNum;
     }
 
-    // Public method named generateNumber()
-    // to generate a random number between 0 and 100 and setting the value of randomNum to the generated random number.
+    // Generate a random number between 0 and 100 and set the randomNum value
     public void generateNumber() {
 
-        // Create a new instance called Random to generate a random number.
         Random random = new Random();
 
-        // Generates a random number between 0 and 100.
+        // Generates random number between 0 and 100
         randomNum = random.nextInt(101);
     }
 
-    // Public class method calls compareNumber(int guess) that accepts an integer as a guess and returns a true or false.
+    // Compares the user's guess to a generated random number
     public boolean compareNumber(int guess) {
 
         if (guess == randomNum) {
 
-            // If the user's guess is equal to randomNum, it will return as true.
+            // Print feedback for user's inputting right number
             System.out.println("Congratulations, you guessed the number!");
             return true;
 
         } else if (guess > randomNum) {
 
-            // If the user's guess is too high compared to the randomNum, it will return as false.
+            // Print feedback for user's inputting their number being too high
             System.out.println("I'm sorry. That guess was too high.");
 
             return false;
 
         } else {
 
-            // If the user's guess is too low compared to the randomNum, it will return as false.
+            // Print feedback for user's inputting their number being too low
             System.out.println("I'm sorry, That guess was too low.");
 
             return false;
         }
     }
+
 }
 
 
