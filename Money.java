0e3ccf92f98ae5056f21d18660711b9790395d5d
JAI-PHX <JAI2150065@MARICOPA.EDU>
2024-05-26 17:28:00 -0700

    Create Money.java

---
+++ Money.java
@@ -0,0 +1,19 @@
+class Money implements Award {
+
+     private double amountWon = 1000;
+
+     private double amountLost = 200;
+
+     // Display true or false messages based on the players input being right or wrong
+     public int displayWinnings(Players player, boolean correctGuess) {
+
+         if(correctGuess) {
+
+             System.out.printf("%s, that is correct! You won $%.1f\n", player.getfirstName(), amountWon);
+             return (int) amountWon;
+         } else {
+             System.out.printf("%s, sorry, that is incorrect! You lost $%.1f\n", player.getfirstName(), amountLost);
+             return (int) -amountLost;
+         }
+    }
+}
